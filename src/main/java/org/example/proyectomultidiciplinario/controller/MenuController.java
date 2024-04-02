0b04ed0c50fd46312ea1f2c78ced682d137fdc99
Maax9o9 <maxrockogenial@gmail.com>
2024-04-02 15:05:27 -0600

    ventanasOT

--- src/main/java/org/example/proyectomultidiciplinario/controller/MenuController.java
+++ src/main/java/org/example/proyectomultidiciplinario/controller/MenuController.java
@@ -1,11 +1,19 @@
 package org.example.proyectomultidiciplinario.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.example.proyectomultidiciplinario.GestorOrdenesApplication;
 import org.example.proyectomultidiciplinario.models.GestorEmpleados;
+
+import java.io.IOException;
 
 public class MenuController {
     private GestorEmpleados gestorEmpleados;
@@ -59,7 +67,19 @@ public class MenuController {
 
     @FXML
     void btnCrearOt(MouseEvent event) {
-
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(GestorOrdenesApplication.class.getResource("ot-view.fxml"));
+        try {
+            Pane root = fxmlLoader.load();
+            Scene scene= new Scene(root);
+            stage.setTitle("Registro de usuarios");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Node source = (Node) event.getSource();
+        stage = (Stage) source.getScene().getWindow();stage.close();
     }
 
     @FXML
